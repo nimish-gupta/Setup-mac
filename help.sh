99b3e5d6f66c9bf78ea1306ecfe56e037c5eebb1
Nimish <nimishgupta18396@gmail.com>
2018-02-27 23:28:45 +0530

    feat: added the help

--- help.sh
+++ help.sh
@@ -1,10 +1,9 @@
 show_help() {
 	echo "
-	Usage: scripts.sh [Options]
-	These script is used to set the development environment for mac OS
-	using following flags
-	Options:
-		-h   															 Used to show the info about various flags
-		-s <git email for the ssh agent>   Used to set the ssh for the env   
-	"
+ Usage: scripts.sh [Options]
+ These script is used to set the development environment for mac OS using following flags
+ Options: 
+ -h                                 Used to show the info about various flags
+ -s <git email for the ssh agent>   Used to set the ssh for the env   
+"
 }
