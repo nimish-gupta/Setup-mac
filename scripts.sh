05c249b763a36b93b33278f96d9745e7a193b14e
Nimish Gupta <gnimish03@gmail.com>
2018-03-06 23:52:14 +0530

    Refactored the directory structure (#2)
    * feat: added the beautify
    
    * feat: added the git install and ssh commands
    
    * feat: added the help
    
    * fix: check for invalid arguments
    
    * feat: added the check_app as util
    
    * feat: add the xcode license
    
    * create generic multiselect options function
    
    * Added the git ignore
    
    * feat: added the symlink
    
    * feat: add the checks
    
    * feat: add the heroku
    
    * feat: added the version

--- scripts.sh
+++ scripts.sh
@@ -1,67 +1,38 @@
-# install xcode
-xcode-select --install
+source help.sh
+source utils.sh
 
-# install brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
+OPTIND=1
 
-# install git
-brew install git
+install_apps() {
+  echo "******************* Installation starts *******************"
+  source install.sh
+  echo "******************* Installation ends *******************"
+}
 
-# install ruby rvm rails
-echo "gem: --no-document" >> ~/.gemrc
-curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --rails
-
-# install wget
-brew install wget --with-libressl
-
-# install chrome 
-wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-open ~/Downloads/googlechrome.dmg
-
-# install vscode 
-wget https://az764295.vo.msecnd.net/stable/f88bbf9137d24d36d968ea6b2911786bfe103002/VSCode-darwin-stable.zip
-open ~/Downloads/VSCode-darwin-stable.zip.download
-open ~/Downloads/Visual\ Studio\ Code.app/
-sudo mv ~/Downloads/Visual\ Studio\ Code.app /Applications/
-
-# install sublime 
-wget https://download.sublimetext.com/Sublime%20Text%20Build%203143.dmg
-open ~/Downloads/Sublime\ Text\ Build\ 3143.dmg.download/
-open ~/Downloads/Sublime\ Text\ Build\ 3143.dmg
-ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
-
-# install zsh
-brew install zsh zsh-completions
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# install iterm 
-wget https://iterm2.com/downloads/stable/iTerm2-3_1_5.zip
-open ~/Downloads/iTerm2-3_1_5.zip.download
-open ~/Downloads/iTerm.app
-echo plugins=(git colored-man colorize github jira vagrant virtualenv pip python brew osx zsh-syntax-highlighting) >> ~/.zshrc
-alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code" >> ~/.zshrc
-
-# install vscode extensions  
-VSCODE_EXTENSIONS="HookyQR.beautify Rubymaniac.vscode-paste-and-indent alefragnani.project-manager chrisdias.vscodeeditorconfig christian-kohler.npm-intellisense christian-kohler.path-intellisense dbaeumer.vscode-eslint donjayamanne.githistory dsznajder.es7-react-js-snippets eamodio.gitlens eg2.tslint eg2.vscode-npm-script felipecaputo.git-project-manager formulahendry.auto-close-tag formulahendry.auto-rename-tag formulahendry.terminal glen-84.sass-lint joelday.docthis jundat95.react-native-snippet kumar-harsh.graphql-for-vscode michelemelluso.code-beautifier ms-vscode.node-debug2 msjsdiag.debugger-for-chrome naumovs.color-highlight neilding.language-liquid rbbit.typescript-hero robertohuertasm.vscode-icons vangware.light-plus-material vsmobile.vscode-react-native waderyan.gitblame wix.vscode-import-cost"
-
-for VSCODE_EXTENSION in $VSCODE_EXTENSIONS
-do
- 	code --install-extension $VSCODE_EXTENSION
-done
-
-# install nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-
-# setting git ssh key
-ssh-keygen -t rsa -b 4096 -C "nimishgupta18396@gmail.com"
-cat ~/.ssh/id_rsa.pub
-eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/id_rsa
-
-# install auto suggestions plugin
-brew install zsh-autosuggestions
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh >> ~/.zshrc
-
-
+if [ "$#" -ne 1 ]; then
+  install_apps
+else
+  while getopts hs:v opt; do
+    case $opt in
+      h)
+        show_help
+        exit 0
+        ;;
+      s)
+        emailForSsh=$OPTARG
+        install_apps
+        exit 0
+        ;;
+      v)
+        echo "Setup-mac 1.0.0"
+        exit 0
+        ;;
+      *)
+        echo "Invalid arguments are present"
+        show_help 
+        exit 1
+        ;;
+    esac
+  done
+fi
 
---
+++ utils.sh
@@ -0,0 +1,29 @@
+beautify() {
+  if [ $# -ne 2 ]
+  then
+    echo "Few arguments are supplied to beautify command"
+    exit 1
+  fi
+  echo "-------$1 installation starts-------"
+  $2
+  echo "-------$1 installation completed-------"
+}
+
+check_brew_app_insalled() {
+  isInstalled=$(brew ls --versions "$1")
+  if [ -z "$isInstalled" ]
+  then
+    beautify $1 $2
+  else
+    echo "$1 is already installed"
+  fi
+}
+
+check_any_app_installed() {
+  if ! [ -x "$(command -v $1)" ]; then
+    beautify $1 $2
+  else
+    echo "$1 is already installed"
+  fi
+}
+
